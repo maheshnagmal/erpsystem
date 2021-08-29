2111b152d13ed0b883943e71cffa19c033138693
Mahesh Nagmal <aboliwalke2@gmail.com>
2021-08-29 00:31:49 -0700

    updated business repo

--- src/main/java/com/erp/createcompany/repo/BusinessProfileRepo.java
+++ src/main/java/com/erp/createcompany/repo/BusinessProfileRepo.java
@@ -1,9 +1,13 @@
 package com.erp.createcompany.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.erp.createcompany.model.BusinessProfile;
 
+@CrossOrigin
+@RepositoryRestResource(collectionResourceRel = "businessProfile",path="businessProfile")
 public interface BusinessProfileRepo extends JpaRepository<BusinessProfile, Integer> {
 
 }
